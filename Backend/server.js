0055df6260244c9d664ea67e5281ef01a2604ecc
Shoaib khan <shoaibmangal@yahoo.com>
2026-09-16 01:34:59 +0500

    backend file updated

--- Backend/server.js
+++ Backend/server.js
@@ -38,5 +38,6 @@ app.use(
     })
 )
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server running on port${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`server running on port${PORT}`));
+module.exports = app;
