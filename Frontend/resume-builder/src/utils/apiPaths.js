532f1208758d2bd1085b4127a9da2ddfd09880bb
Shoaib khan <shoaibmangal@yahoo.com>
2026-09-17 00:59:37 +0500

    backend url added to frontend

--- Frontend/resume-builder/src/utils/apiPaths.js
+++ Frontend/resume-builder/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://fyp-resumify-cv-maker-i7kv.vercel.app";
+export const BASE_URL = "https://fyp-resumify-cv-maker-k586.vercel.app";
 
 // utils/apiPath.js
 export const API_PATHS = {
